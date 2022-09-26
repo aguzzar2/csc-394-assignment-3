780e132abe34d3b5a1022117b0ce6d23e68f51ac
Anthony Guzzardo <113217959+aguzzar2@users.noreply.github.com>
2022-09-26 12:33:34 -0500

    index

--- hw3.py
+++ hw3.py
@@ -1,7 +1,7 @@
 from flask import Flask,render_template
 
 app= Flask(__name__)
-@app.route('/')
+@app.route('/hw3')
 
 def main():
-    return render_template(index.html)
+    return render_template('index.html')
